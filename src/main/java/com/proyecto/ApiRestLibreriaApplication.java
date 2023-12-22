99a219073da709c286a838b7a02fe47b5d7c7d08
JulioVargas26 <84108359+JulioVargas26@users.noreply.github.com>
2023-12-21 19:55:50 -0500

    postgres

--- src/main/java/com/proyecto/ApiRestLibreriaApplication.java
+++ src/main/java/com/proyecto/ApiRestLibreriaApplication.java
@@ -3,11 +3,15 @@ package com.proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.apachecommons.CommonsLog;
+
 @SpringBootApplication
+@CommonsLog
 public class ApiRestLibreriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestLibreriaApplication.class, args);
+		log.info("¡Terminó de cargar spring ... Fuerza!");
 	}
 
 }
